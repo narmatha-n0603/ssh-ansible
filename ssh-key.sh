1a0390cbd323edd7fef84f83804e18b9eca3513f
narmatha-n0603 <narmathan1@hexaware.com>
2024-07-12 13:54:58 +0530

    Update ssh-key.sh

--- ssh-key.sh
+++ ssh-key.sh
@@ -4,22 +4,6 @@ echo "Starting ssh-key.sh script at $(date)"
 INVENTORY_SCRIPT="./aws_ec2.yaml"
 PUBLIC_KEY_PATH="/var/lib/jenkins/.ssh/id_rsa.pub"
 SSH_USER="jenkins"
-if ! command -v yq &> /dev/null; then
-    echo "yq command not found. Please install yq."
-    exit 1
-fi
-if [ -z "$INVENTORY_SCRIPT" ] || [ ! -f "$INVENTORY_SCRIPT" ]; then
-    echo "INVENTORY_SCRIPT is not set or file does not exist."
-    exit 1
-fi
-if [ -z "$PUBLIC_KEY_PATH" ] || [ ! -f "$PUBLIC_KEY_PATH" ]; then
-    echo "PUBLIC_KEY_PATH is not set or file does not exist."
-    exit 1
-fi
-if [ -z "$SSH_USER" ]; then
-    echo "SSH_USER is not set."
-    exit 1
-fi
 HOSTS=$(yq e '.all.hosts[]' "$INVENTORY_SCRIPT")
 for HOST in $HOSTS; do
     echo "Copying SSH key to $HOST"
