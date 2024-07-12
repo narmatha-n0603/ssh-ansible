803c0d93b0865014ef4fa5d68ccd2c9b2e938beb
narmatha-n0603 <narmathan1@hexaware.com>
2024-07-12 10:05:15 +0530

    Delete copy-ssh-key.sh

--- copy-ssh-key.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-INVENTORY_SCRIPT="./aws_ec2.yaml"
-PUBLIC_KEY_PATH="/var/lib/jenkins/.ssh/id_rsa.pub"
-SSH_USER="jenkins" 
-HOSTS=$(yq e '.all.hosts[]' $INVENTORY_SCRIPT)
-for HOST in $HOSTS; do
-    ssh-copy-id -i "$PUBLIC_KEY_PATH" "$SSH_USER@$HOST"
-done
